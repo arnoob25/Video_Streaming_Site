5c7ba712f93e91669f28a8429a862045c91a7f57
Arnob <arnob@gmail.com>
2024-01-04 08:40:37 +0600

    Refactored the StreamVIdeo CBV

--- Video_Streaming_Site/video_streaming/views.py
+++ Video_Streaming_Site/video_streaming/views.py
@@ -2,22 +2,22 @@ from django.views.generic import DetailView
 from django.views.generic.edit import FormMixin
 from django.urls import reverse
 from . import models, forms
+from django.shortcuts import redirect
 
 class StreamVideo(FormMixin, DetailView):
     model = models.Video
     form_class = forms.CommentForm
     template_name = 'stream.html'
     
-    def get_success_url(self):
-        return reverse('video_streaming:stream', kwargs={'slug': self.object.slug})
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['comments'] = models.Comment.objects.filter(video=self.object).order_by('-created_at')
         return context
 
     def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
+        if not request.user.is_authenticated:
+            return redirect('login')
+        self.object = self.get_object() 
         form = self.get_form()
         return self.form_valid(form) if form.is_valid() else self.form_invalid(form)
 
@@ -27,3 +27,7 @@ class StreamVideo(FormMixin, DetailView):
         comment.video = self.object
         comment.save()
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('video_streaming:stream', kwargs={'slug': self.object.slug})
+
